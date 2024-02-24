00c41f99b6dd5f7e6eb0d4928a3e3dcd54b1072a
SarahIrons <sarahirons11@gmail.com>
2024-02-24 12:17:49 -0500

    Add project files.

---
+++ ParametersAssignment/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//PARAMETERS ASSIGNMENT
+
+//Perform these actions and create a console app that includes the following:
+
+
+//Make the Employee class take a generic type parameter.
+
+//Add a property to the Employee class called “things” and have its data type be a generic list matching the generic type of the class.
+
+//Instantiate an Employee object with type “string” as its generic parameter. Assign a list of strings as the property value of Things.
+
+//Instantiate an Employee object with type “int” as its generic parameter. Assign a list of integers as the property value of Things.
+
+//Create a loop that prints all of the Things to the Console.
+namespace ParametersAssignment
+{
+    public class Employee<T> //the angle bracket is called a type parameter
+    {
+        public List<T> Things; //setting up to accept a list of the generic Type
+       
+        public Employee(List<T> ThingsPassedAsArg)//this is set up to take any data type
+        {
+            this.Things = ThingsPassedAsArg;
+        }
+    }
+
+}
+
+
+
+
+
+
+
---
+++ ParametersAssignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic; //have to import to use "Generic" and idea of list
+
+namespace ParametersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> Names = new List<string>(); //declare a list of strings and add names(items) to list.
+            Names.Add("Tom");
+            Names.Add("Dick");
+            Names.Add("Harry");
+
+            List<int> Ids = new List<int>(); //declare list of integers
+            Ids.Add(101);
+            Ids.Add(102);
+            Ids.Add(103);
+
+            Employee<string> EmpNames = new Employee<string>(Names); //first instance of generics data "things" using string
+            Employee<int> EmpIdNums = new Employee<int>(Ids); //second instance of data type "things" using integer
+
+           foreach (string i in EmpNames.Things)
+            {
+                Console.WriteLine(i);
+            }
+            foreach (int i in EmpIdNums.Things)//this is written nearly the same as above loop but just changed out variable id nums instead of names
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+    
+}
+
+
